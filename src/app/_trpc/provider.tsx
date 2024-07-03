b202d637b709e99a44c43e752a272025fe1b3a36
matsumoto <dsongben@gmail.com>
2024-07-03 22:19:56 +0900

    providerの修正

--- src/app/_trpc/provider.tsx
+++ src/app/_trpc/provider.tsx
@@ -3,16 +3,20 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { httpBatchLink } from "@trpc/client";
 
 import { clientApi } from "./client-api";
+import { useState } from "react";
 
 export default function Provider({ children }: { children: React.ReactNode }) {
-  const queryClient = new QueryClient({});
-  const trpcClient = clientApi.createClient({
-    links: [
-      httpBatchLink({
-        url: "http://localhost:3000/api/trpc",
-      }),
-    ],
-  });
+  // コンポーネントがレンダリングされる度にインスタンスを生成するとパフォーマンスが落ちるため、useStateで定義する
+  const [queryClient] = useState(() => new QueryClient({}));
+  const [trpcClient] = useState(() =>
+    clientApi.createClient({
+      links: [
+        httpBatchLink({
+          url: "http://localhost:3000/api/trpc",
+        }),
+      ],
+    })
+  );
   return (
     <clientApi.Provider client={trpcClient} queryClient={queryClient}>
       <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
