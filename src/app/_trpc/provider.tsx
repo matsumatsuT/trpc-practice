8a4940445bd680aba04428803b4d96313e7e0d1e
matsumoto <dsongben@gmail.com>
2024-07-27 17:46:42 +0900

    本番用のapi追加

--- src/app/_trpc/provider.tsx
+++ src/app/_trpc/provider.tsx
@@ -1,25 +1,31 @@
-"use client";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { httpBatchLink } from "@trpc/client";
+"use client"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { httpBatchLink } from "@trpc/client"
 
-import { clientApi } from "./client-api";
-import { useState } from "react";
+import { clientApi } from "./client-api"
+import { useState } from "react"
+
+const apiUrl =
+  process.env.NODE_ENV === "production"
+    ? process.env.NEXT_PUBLIC_SUPABASE_URL ?? ""
+    : "http://localhost:3000/api/trpc"
 
 export default function Provider({ children }: { children: React.ReactNode }) {
   // コンポーネントがレンダリングされる度にインスタンスを生成するとパフォーマンスが落ちるため、useStateで定義する
-  const [queryClient] = useState(() => new QueryClient({}));
+  const [queryClient] = useState(() => new QueryClient({}))
   const [trpcClient] = useState(() =>
     clientApi.createClient({
       links: [
         httpBatchLink({
-          url: "http://localhost:3000/api/trpc",
+          url: apiUrl,
         }),
       ],
     })
-  );
+  )
+
   return (
     <clientApi.Provider client={trpcClient} queryClient={queryClient}>
       <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
     </clientApi.Provider>
-  );
+  )
 }
